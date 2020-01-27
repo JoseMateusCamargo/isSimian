e646d10b37bfcb41b7446804bcedf8d57ed935e9
Jose Mateus Camargo de Leva <47319248+JoseMateusCamargo@users.noreply.github.com>
2020-01-27 14:44:22 -0300

    Update connectionFactory.js

--- nodeApi/persistencia/connectionFactory.js
+++ nodeApi/persistencia/connectionFactory.js
@@ -3,9 +3,9 @@ var mysql = require('mysql');
 function createDBConnection(){
     return mysql.createConnection({
         host: 'localhost',
-        user: 'pfwkfmqh_laravel',
-        password: 'q1w2e3r4',
-        database: 'pfwkfmqh_issimian'
+        user: 'root',
+        password: '',
+        database: 'issimian'
     });
 }
 
